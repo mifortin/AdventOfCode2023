0970cab6143eb8e974c8f178024da5f50b8a35af
Michael <quirky-clearer-0c@icloud.com>
2023-12-05 18:48:57 -0500

    Initial Commit

---
+++ AdventOfCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdventOfCode
+//
+//  Created by Michael Fortin on 2023-12-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
